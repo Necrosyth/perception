070094d17e80484f93815ba9774a03c3ad0dad14
Necrosyth <acoustify69420@gmail.com>
2026-08-31 03:07:56 +0530

    chore: stop tracking Docusaurus dev cache

--- docs/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/necro/Desktop/new_surv/final_surv/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/home/necro/Desktop/new_surv/final_surv/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/necro/Desktop/new_surv/final_surv/docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/necro/Desktop/new_surv/final_surv/docs/src/css/custom.css"),
-];
